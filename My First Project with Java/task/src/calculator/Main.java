b39a5f3cc0835c049aaa4c6a25b7061703882d17
hsezgin <h.sezgin.b@gmail.com>
2024-02-04 01:28:21 +0300

    first

---
+++ My First Project with Java/task/src/calculator/Main.java
@@ -0,0 +1,31 @@
+package calculator;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("""
+                Earned amount:
+                Bubblegum: $202
+                Toffee: $118
+                Ice Cream: $2250
+                Milk chocolate: $1680
+                Doughnut: $1075
+                Pancake: $80
+
+                """);
+        int income = 5405;
+        System.out.printf("Income: $%d\n", income);
+        System.out.println("Staff expenses:");
+        Scanner scanner1 = new Scanner(System.in);
+        //System.out.print(">");
+        int staffExpenses = scanner1.nextInt();
+        System.out.println("Other expenses:");
+        //System.out.print(">");
+        Scanner scanner2= new Scanner(System.in);
+        int otherExpenses = scanner2.nextInt();
+        int netIncome = income - (staffExpenses + otherExpenses);
+        System.out.printf("Net income: $%d", netIncome);
+    }
+}
